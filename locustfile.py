a3ff44729cc6db8a4f1f00c473ed2a465e3a520b
tomasgp97 <tomas.gonzalezp97@gmail.com>
2020-06-10 17:55:26 -0300

    fixed el bug flasherovich de fabri

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, between, task, TaskSet
+from locust import HttpUser, between, task, TaskSet, SequentialTaskSet
 
 
 class GetCartPriceUser(HttpUser):
@@ -7,10 +7,8 @@ class GetCartPriceUser(HttpUser):
 
     @task
     def get_cart_price(self):
-        checkout_information_for_address = {"products": [5],
-                                            "address": 3}
-        checkout_information_for_another_address = {"products": [5, 5, 5],
-                                                    "address": 4}
+        checkout_information_for_address = {"products": [5], "address": 3}
+        checkout_information_for_another_address = {"products": [5], "address": 4}
 
         self.client.post("/cart", json=checkout_information_for_address)
         self.client.post("/cart", json=checkout_information_for_another_address)
@@ -25,35 +23,22 @@ class GetAllCartsUser(HttpUser):
         self.client.get("/products/")
 
 
+class SequentialTasks(SequentialTaskSet):
+    @task
+    def add_product_to_cart(self):
+        self.client.put("/users/1/cart/add/5")
+
+    @task
+    def remove_product_from_cart(self):
+        self.client.put("/users/1/cart/remove/5")
+
+
 class InteractWithCartUser(HttpUser):
-    wait_time = between(0, 1)
+    wait_time = between(0, 3)
     weight = 3
 
-    @task(6)
-    def add_product_to_cart(self):
-        self.client.put("/1/cart/add/5")
-
-    @task(4)
-    def add_product_to_cart(self):
-        self.client.put("/2/cart/remove/5")
-
-
-# class CheckoutTasks(TaskSet):
-#     wait_time = between(0, 1)
-
-# @task
-# def create_transactions(self):
-#     username_password = AUTH_MAP[random.randint(1, 4)]
-#     self.client.post('/auth', json=username_password)
-#     transaction_body = {
-#         'userFrom': USER_MAP[random.randint(1, 4)],
-#         'userTo': USER_MAP[random.randint(1, 4)],
-#         'amount': random.randint(0, 20)
-#     }
-#     self.client.post("/transactions", json=transaction_body)
-#
-# def on_stop(self):
-#     self.interrupt()
-
-# @task
-# def add_products_to_cart(self):
+    tasks = [SequentialTasks]
+    # @task
+    # def add_then_remove_product(self):
+    #     self.client.put("http://127.0.0.1:8080/users/1/cart/add/5")
+    #     self.client.put("http://127.0.0.1:8080/users/1/cart/remove/5")
